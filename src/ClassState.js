14a17f1a0f0c9fc46a60b48a4b0bd289f1ea975e
AyrielNoriega <anoriega2310@gmail.com>
2022-08-15 22:55:50 -0500

    Estados
    Estados simples: React.Component vs. useState

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 
 class ClassState extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            error: true,
+            otroEstado: false
+        }
+    }
     render(){
         return(
             <div>
-                <h2>Eliminar ClassState</h2>
+                <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor, escribe el código de seguridad.</p>
-
+                {this.state.error && (
+                    <p>Error: el código es incorrecto.</p>
+                )}
                 <input
                     placeholder="codigo de seguridad"
                 />
-                <button>Compropar</button>
+                <button
+                    //onClick={() => this.setState({error: !this.state.error})}
+                    onClick={() => this.setState(prevState => ({error: !prevState.error}))}
+                >Compropar</button>
             </div>
         );
     }
--- src/UseState.js
+++ src/UseState.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function UseState() {
+function UseState(props) {
+    const [error, setError] = React.useState(false);
+    const [otroState, setOtroState] = React.useState(false);
+
     return(
         <div>
-            <h2>Eliminar ClassState</h2>
+            <h2>Eliminar {props.name}</h2>
             <p>Por favor, escribe el código de seguridad.</p>
 
+            {error && (
+                <p>Error: el código es incorrecto.</p>
+            )}
             <input
                 placeholder="codigo de seguridad"
             />
