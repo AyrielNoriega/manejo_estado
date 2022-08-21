0eb34f2675f44fb1ee9b63edca1c844855e04f47
AyrielNoriega <anoriega2310@gmail.com>
2022-08-21 15:17:12 -0500

    Estados declarativos con useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { UseState } from "./UseState";
 import { ClassState } from "./ClassState";
+import { UseReducer } from "./UseReducer";
 
 function App() {
   return (
     <div className="App">
        <UseState name="UseState"/>
        <ClassState name="Classtate"/>
+       <UseReducer name="Use Reducer"/>
     </div>
   );
 }
--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,5 +1,109 @@
+import React from "react";
+
+const SECURITY_CODE = 'paradigma';
+
+function UseReducer(props) {
+
+    //Estados compuestos con useState
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    console.log(state);
+    React.useEffect(() => {
+        console.log('empezando efecto');
+    
+        if (state.loading) {
+            setTimeout(() => {
+                console.log('haciendo consulta');
+                //setLoading(false);
+
+                if (state.value === SECURITY_CODE) {
+                    dispatch({
+                        type: 'CONFIRM'
+                    });
+                } else {
+                    dispatch({
+                        type: 'ERROR'
+                    });
+                }
+
+                console.log('terminando consulta');
+            }, 3000);
+        }
+    
+        console.log('terminando efecto');
+    }, [state.loading])
+
+    if (!state.deleted && !state.confirmed) {
+        return(
+            <div>
+                <h2>Eliminar {props.name}</h2>
+                <p>Por favor, escribe el código de seguridad.</p>
+    
+                {(state.error && !state.loading) && (
+                    <p>Error: el código es incorrecto.</p>
+                )}
+                {state.loading && (
+                    <p>Cargando...</p>
+                )}
+                <input
+                    placeholder="codigo de seguridad"
+                    value={ state.value }
+                    onChange={(event) => {
+                        dispatch({ 
+                            type: 'WRITE',
+                            payload: event.target.value
+                        });
+                       //onWrite(event.target.value)
+                    }}
+                    
+                />
+                <button
+                    onClick={()=> {
+                            dispatch({ type: 'CHECK' });
+                           // onCheck();
+                        }
+                        //setLoading(true)
+                    }
+                >Compropar</button>
+            </div>
+        );
+    } else if (state.confirmed && !state.deleted) {
+        return (
+            <React.Fragment>
+                <p>Estado de confirmación</p>
+                <button
+                    onClick={() => {
+                        dispatch({ type: 'DELETE' });
+                        //onDelete();
+                    }}
+                >Sí, eliminar</button>
+                <button
+                    onClick={() => {
+                        dispatch({ type: 'RESET' });
+                        //onReset();
+                    }}
+                >No, me arrepentí</button>
+            </React.Fragment>
+        );
+    } else {
+        return (
+            <React.Fragment>
+                <p>Eliminado con éxito</p>
+                <button
+                    onClick={() => {
+                        dispatch({ type: 'RESET' });
+                        //onReset();
+                    }}
+                >
+                    Resetear, volver atrás
+                </button>
+            </React.Fragment>
+        );
+    }
+}
+
+//lo primero que necesitamos para nuestros reducer son estados compuestos. 
 const initialState = {
-    value: '',
+    value: 'paradigma',
     error: false,
     loading: false,
     deleted: false,
@@ -10,6 +114,8 @@ const initialState = {
 // }
 
 // FORMAS DE CREAR LOS REDUCER
+
+//los reducer reciben dos parámetros, el state y el action
 
 //reducer if
 const reducerIf = (state, action) => {
@@ -55,7 +161,13 @@ const reducerSwith = (state, action) => {
 }
 
 //reducer swith
-const reducerObject = (state) => ({
+const reducerObject = (state, payload) => ({
+    CONFIRM: {
+        ...state,
+        loading: false,
+        error: false,
+        confirmed: true
+    },
     ERROR: {
         ...state,
         error: true,
@@ -65,13 +177,28 @@ const reducerObject = (state) => ({
         ...state,
         loading: true
     },
+    DELETE:{
+        ...state,
+        deleted: true
+    },
+    RESET: {
+        ...state,
+        confirmed: false,
+        deleted: false,
+        value: ''
+    },
+    WRITE: {
+        ...state,
+        value: payload
+    }
 })
 
 const reducer = (state, action) => {
     if (reducerObject(state)[action.type]) {
-        return reducerObject(state)[action.type];
+        return reducerObject(state, action.payload)[action.type];
     } else {
         return state;
     }
 }
 
+export { UseReducer };
